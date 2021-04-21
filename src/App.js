db7c501d41ae074260cf1ec5b97dd55e256e6e63
Sitthiphum Naruephankulchai <sitthiphum.n@kbtg.tech>
2021-04-21 12:43:30 +0700

    master

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-function App() {
-  return (
-    <div>
-      <h2>Hello React ...</h2>
-    </div>
-  );
-}
+import logo from './logo.svg';
+import './App.css';
+import { Component } from "react";
+import { renderIntoDocument } from 'react-dom/test-utils';
 
+class App extends Component{
+  render(){
+    return (
+      <div>
+        <h2> Hello React from class</h2>
+      </div>
+    );
+  }
+}
 export default App;
