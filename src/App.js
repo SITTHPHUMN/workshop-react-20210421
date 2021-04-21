6462fdcfb018a738adc6e9e50a834ee8131116b5
Sitthiphum Naruephankulchai <sitthiphum.n@kbtg.tech>
2021-04-21 14:12:48 +0700

    master

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Hello React from class ...</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
